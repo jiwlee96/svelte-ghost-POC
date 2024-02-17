403625ed7700f6ec407334357ec94b0b42b937c9
Peter Lee <25555982+jiwlee96@users.noreply.github.com>
2024-02-17 17:47:39 -0600

    added flowbite but removing soon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,23 @@
-<script>
-  import '../app.css'
+<script lang="ts">
+	import '../app.css';
+	import Icon from '$lib/assets/favicon.png';
+	import { Navbar, NavBrand, NavHamburger, NavUl, NavLi } from 'flowbite-svelte';
+	import NavContainer from 'flowbite-svelte/NavContainer.svelte';
 </script>
 
-<nav>
-  <a href="/">Home</a>
-  <a href="/about">About</a>
-</nav>
+<Navbar class="bg-transparent border-transparent dark:bg-transparent dark:border-transparent">
+	<NavBrand href="/">
+		<img src={Icon} class="me-3 h-6 sm:h-9" alt="Svelte Logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Kairos Fellowship Church</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl>
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/about">About</NavLi>
+		<NavLi href="/docs">Navbar</NavLi>
+		<NavLi href="/pricing">Pricing</NavLi>
+		<NavLi href="/contact">Contact</NavLi>
+	</NavUl>
+</Navbar>
 
-<slot></slot>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from 'flowbite-svelte';
+</script>
+
+<Button>Default</Button>
+<Button class="!bg-blue-500">Blue</Button>
+<Button class="bg-green-500">Green</Button>
