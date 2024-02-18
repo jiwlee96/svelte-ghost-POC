470af760eccc61661529e93c63e54be4c84e298d
Peter Lee <25555982+jiwlee96@users.noreply.github.com>
2024-02-17 20:22:00 -0600

    removed flowbite but added unplugin and started writing dropdown menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import '../app.css';
-	import Icon from '$lib/assets/favicon.png';
-	import { Navbar, NavBrand, NavHamburger, NavUl, NavLi } from 'flowbite-svelte';
-	import NavContainer from 'flowbite-svelte/NavContainer.svelte';
+	import IconMenu from '~icons/material-symbols-light/menu';
+	import IconClose from '~icons/material-symbols-light/close';
+
+	let dropdown = false;
+
+	function handleDropdown() {
+		dropdown = !dropdown
+	} 
 </script>
 
-<Navbar class="bg-transparent border-transparent dark:bg-transparent dark:border-transparent">
+<!-- <Navbar class="bg-transparent border-transparent dark:bg-transparent dark:border-transparent">
 	<NavBrand href="/">
 		<img src={Icon} class="me-3 h-6 sm:h-9" alt="Svelte Logo" />
 		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Kairos Fellowship Church</span>
@@ -18,6 +23,30 @@
 		<NavLi href="/pricing">Pricing</NavLi>
 		<NavLi href="/contact">Contact</NavLi>
 	</NavUl>
-</Navbar>
+</Navbar> -->
+
+<nav class="bg-transparent">
+	<div class="bg-white mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+		<button on:click={handleDropdown}>
+			{#if dropdown}
+				<IconClose />
+			{:else}
+				<IconMenu />
+			{/if}
+		</button>
+		{#if dropdown}
+		Hey
+		{/if}
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="about">About</a>
+			</li>
+		</ul>
+	</div>
+</nav>
+
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { Button } from 'flowbite-svelte';
+
 </script>
 
-<Button>Default</Button>
-<Button class="!bg-blue-500">Blue</Button>
-<Button class="bg-green-500">Green</Button>
+<p>Default</p>
+<p class="!bg-blue-500">Blue</p>
+<p class="bg-green-500">Green</p>
